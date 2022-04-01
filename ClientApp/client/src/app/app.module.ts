d712a69e12507923e9d513af48fce1e7993ebc3d
Jude Valerius <jude_valerius@hotmail.com>
2022-04-01 00:50:08 -0400

    Upgrade to Angular 12 and fixed the dropdown menu issue.

--- ClientApp/client/src/app/app.module.ts
+++ ClientApp/client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './users/user-list/user-list.component';
 import { NavMenuComponent } from './nav-menu/nav-menu/nav-menu.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home/home.component';
 import { SittersComponent } from './sitters/sitters/sitters.component';
 import { RegisterComponent } from './register/register/register.component';
 import { RegisterinfoComponent } from './register/registerinfo/registerinfo.component';
 import { MembercardComponent } from './users/membercard/membercard.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -31,8 +33,11 @@ import { MembercardComponent } from './users/membercard/membercard.component';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ClientApp/client/src/app/services/users.service.ts
+++ ClientApp/client/src/app/services/users.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -14,16 +14,11 @@ export class UsersService {
   constructor(private http: HttpClient) { }
 
   getUsers(){
-    const httpOptions = {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('appUser')).token
-      })
-    }
-    
+
     if(this.users.length>0)
       return of(this.users);
  
-    return this.http.get<User[]>('https://localhost:5001/api/users', httpOptions).pipe(
+    return this.http.get<User[]>('https://localhost:5001/api/users').pipe(
       map((usrs:User[]) =>{
         this.users = usrs;
         return usrs;
